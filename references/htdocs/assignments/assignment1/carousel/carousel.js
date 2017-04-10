daee9381eb5b6451fad67e3f1914ac95b83e9bff
kazuma-sato <kazuma.sato@georgebrown.ca>
2017-04-09 22:06:33 -0400

    Cleaning things up

--- references/htdocs/assignments/assignment1/carousel/carousel.js
+++
@@ -1,9 +0,0 @@
-var fishing_pics;
-
-jsLib.event.add( window, "load", function() {
-    var options = {
-        height: 500,
-        maxSpeed: 5
-    }
-    fishing_pics = new Carousel("fishing_pics", options);
-});
